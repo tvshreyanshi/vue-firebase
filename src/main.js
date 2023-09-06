5c0f087b260860e8ad6fe89cfc5040487e333d15
tvshreyanshi <shreyanshi@techvoot.com>
2023-09-06 10:17:54 +0530

    blog-post

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue';
 import Vue2Editor from 'vue2-editor';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+library.add(fas);
+library.add(fab);
+
 createApp(App).use(store).use(router).use(Vue2Editor)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import BlogsView from '../views/BlogsView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/blogs',
+    name: 'Blogs',
+    component: BlogsView,
   },
 ];
 
